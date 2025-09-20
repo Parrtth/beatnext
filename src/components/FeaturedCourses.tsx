16c945a12231115e2e0e7f52ddf9ac72c971a297
parth <patelparth7719@gmail.com>
2025-09-20 17:28:17 +0530

    chore: fix hero section and json data

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import courseData from "../data/music_courses.json"
 
-const FeaturedCourses = () => {
-  return (
-    <div>FeaturedCourses</div>
-  )
+
+function FeaturedCourses() {
+    return(
+        <div className=" py-12 bg-gray-900">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            
+            </div>
+    )
 }
 
 export default FeaturedCourses
